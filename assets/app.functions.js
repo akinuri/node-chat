6fb5aa3da3e2ac17f2e6d0f2c2c56b4453de2926
Noreh AD <akinoreh@gmail.com>
2022-07-27 22:00:37 +0300

    ui: send message

---
+++ assets/app.functions.js
@@ -0,0 +1,75 @@
+function getCurrentTime() {
+    let d = new Date();
+    let time = [
+        d.getHours().toString().padStart(2, 0),
+        d.getMinutes().toString().padStart(2, "0"),
+    ].join(":");
+    return time;
+}
+
+function collectMessage() {
+    let message = {
+        from : {
+            user : $("#user").val(),
+            time : getCurrentTime(),
+        },
+        message : $("#messageBox").val(),
+    };
+    return message;
+}
+
+function createChatEntry(message, isSelf = false) {
+    let entry = `
+    <div class="entry">
+        <div class="from">
+            <div class="user">${message.from.user}</div>
+            ·
+            <div class="time">${message.from.time}</div>
+        </div>
+        <div class="message">${message.message}</div>
+    </div>
+    `;
+    let tempEl = document.createElement("div");
+    tempEl.innerHTML = entry;
+    entry = tempEl.children[0].cloneNode(true);
+    if (isSelf) {
+        entry.classList.add("self");
+    }
+    return entry;
+}
+
+function writeMessage(message, isSelf = false) {
+    if (!message.from.user || !message.message) {
+        return null;
+    }
+    let entry = createChatEntry(message, isSelf);
+    let historyElement = $("#history");
+    let shouldScroll = isScrolledToBottom(historyElement.get(0));
+    historyElement.append(entry);
+    if (isSelf || shouldScroll) {
+        entry.scrollIntoView();
+    }
+    return entry;
+}
+
+function isScrolledToBottom(el) {
+    return el.scrollTop + el.clientHeight > el.scrollHeight - (el.clientHeight / 2);
+}
+
+function sendMessage() {
+    let isSent = false;
+    // TODO: ??
+    return isSent;
+}
+
+// fake; lets test the scroll on message receive
+function receiveMessage() {
+    let message = {
+        from : {
+            user : "Shia",
+            time : getCurrentTime(),
+        },
+        message : "Just do it!",
+    };
+    writeMessage(message);
+}
---
+++ assets/app.js
@@ -0,0 +1,22 @@
+function initSendMessage() {
+    let message = collectMessage();
+    let entry = writeMessage(message, true);
+    // sendMessage(message, (isSent) => {
+    //     if (!isSent) {
+    //         entry.remove();
+    //     }
+    // });
+}
+
+$("#sendMessageButton").on("click", initSendMessage);
+
+$("#messageBox").on("keydown", (e) => {
+    e = e.originalEvent;
+    if (e.code == "Enter" && e.ctrlKey) {
+        initSendMessage();
+    }
+});
+
+$(document).ready(() => {
+    $("#history").css("height", $("#history").css("height"));
+});
